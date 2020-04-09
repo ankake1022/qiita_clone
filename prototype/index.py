6765b7992c2f8eb089005c45c526b673a7c58439
ankake1022 <ankae_1022@i.softbank.jp>
2020-04-09 11:18:04 +0900

    add hover_action

--- prototype/index.py
+++ prototype/index.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+login_status = False
+
 @app.route('/')
 def home():
-    return render_template("home.html")
+    title = "home"
+    return render_template("home.html", title=title, login_status=login_status)
 
 @app.route('/search')
 def serch():
-    return render_template("search.html")
+    title = "search"
+    return render_template("search.html", title=title, login_status=login_status)
 
 #PORTがデフォルトだと5000のままなので5500に書き直す
 #(・・・外部から読み込んでいるため？)
